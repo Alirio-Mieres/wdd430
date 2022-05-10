cdee6370749cf6470787a55cd32d0c2ee3b47dfa
Alirio-Mieres <mie21001@byui.edu>
2022-05-10 17:09:17 -0400

    update

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -7,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactDetailComponent implements OnInit {
 
+  @Input() contact: Contact;
+  detail: Contact;
+  
   constructor() { }
 
   ngOnInit(): void {
---
+++ cms/src/app/messages/message-list/message-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
+
+@Component({
+  selector: 'cms-message-list',
+  templateUrl: './message-list.component.html',
+  styleUrls: ['./message-list.component.css']
+})
+export class MessageListComponent implements OnInit {
+  
+  messages: Message[] = [
+    new Message(1, 'Steven Johnson', 'The grades for this assignment have been posted', 'Bro. Jackson'),
+    new Message(1, 'Bro. Jackson', 'When is assignment 3 due', 'Steven Johnson'),
+    new Message(1, 'Mark Smith', 'Assignment 3 is due on Saturday at 11:30 pm', 'Bro. Jackson')
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
+
+}
+
