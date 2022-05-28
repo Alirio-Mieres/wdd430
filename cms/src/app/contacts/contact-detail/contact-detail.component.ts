e7aadc6da4cf7ad633b508ba0015c4e380e44635
Alirio-Mieres <mie21001@byui.edu>
2022-05-28 06:55:21 -0400

    week 6

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Contact } from '../contact.model';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'cms-contact-detail',
@@ -10,10 +12,26 @@ export class ContactDetailComponent implements OnInit {
 
   @Input() contact: Contact;
   detail: Contact;
+  id: string;
   
-  constructor() { }
+  constructor(
+    private contactService: ContactService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['id'];
+        this.contact = this.contactService.getContact(this.id);
+      }
+    )
+  }
+
+  onDelete(){
+    this.contactService.deleteContact(this.contact);
+    this.router.navigate[('/contacts')];
   }
 
 }
